f3ceb5bcf85a287bc790a3dd72e244f7468db055
Robinson Alcantara <robinson.elduro@hotmail.com>
2024-09-03 00:27:35 -0400

    Mejora de Vista

--- js/app.js
+++ js/app.js
@@ -1,141 +1,52 @@
+
+//Selectores
 const pantalla=document.querySelector(".pantalla");
-const n1 =document.querySelector("[value='1']");
-const n2 =document.querySelector("[value='2']");
-const n3 =document.querySelector("[value='3']");
-const n4 =document.querySelector("[value='4']");
-const n5 =document.querySelector("[value='5']");
-const n6 =document.querySelector("[value='6']");
-const n7 =document.querySelector("[value='7']");
-const n8 =document.querySelector("[value='8']");
-const n9 =document.querySelector("[value='9']");
-const n0 =document.querySelector("[value='0']");
-const mas =document.querySelector("[value='+']");
-const menos =document.querySelector("[value='-']");
-const mult =document.querySelector("[value='*']");
-const div =document.querySelector("[value='/']");
-const igual =document.querySelector("[value='=']");
-const borrar =document.querySelector("[value='C']");
+const botones = document.querySelectorAll(".boton");
 
+//Recoore el arreglo de botones
+botones.forEach(boton=>{
 
-//Event Listener
-n1.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
+    boton.addEventListener("click",()=>{
+        btnPresionado=boton.value;
+
+        //Si la pantalla tiene menos caracteres sigue el programa
+        if(pantalla.textContent.length<10){
+
+            if(btnPresionado==="="){
+                try{
+                pantalla.textContent=(Math.round(eval(pantalla.textContent)*100)/100);
+                }
+                catch{
+                    pantalla.textContent="Error!"
+                }
+                return;
+            } 
+
+            if(btnPresionado==="C"){
+                pantalla.textContent="0";
+                return;
+            }
+
+            if(btnPresionado==="borrar"){
+                if(pantalla.textContent.length===1 || pantalla.textContent==="Error!"){
+                    pantalla.textContent="0";
+                    
+                }else{
+                    pantalla.textContent=pantalla.textContent.slice(0, pantalla.textContent.length-1);
+                }            
+                return;
+            }
+
+            if(pantalla.textContent==="0" || pantalla.textContent==="Error!"){
+                pantalla.textContent=btnPresionado;
+            }else{
+                pantalla.textContent+=btnPresionado;
+            }
+
+        }else{
+            pantalla.textContent="Error!"
+        }
+    })    
+
 })
-n2.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n3.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n4.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n5.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n6.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n7.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n8.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n9.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-n0.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-mas.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-menos.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-mult.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-div.addEventListener("click",(e)=>{
-    if(pantalla.textContent==="0"){
-        pantalla.textContent=e.target.value;
-    }else{
-        pantalla.textContent+=e.target.value;
-    }
-    
-})
-igual.addEventListener("click",(e)=>{
-    
-   pantalla.textContent=eval(pantalla.textContent);
-    
-})
-borrar.addEventListener("click",()=>{
-    pantalla.textContent=0;   
-    
-})
+
